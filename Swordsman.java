d48dc621362de9f8e598e1876dc3b011cde63992
Grazielle Ann <grazielleannagubang819@gmail.com>
2025-10-07 11:13:29 +0800

    Add files via upload

---
+++ Swordsman.java
@@ -0,0 +1,17 @@
+public class Swordsman extends Character {
+
+    public Swordsman(String name) {
+        super(name);
+        this.health = 170;
+    }
+
+    public void slash() {
+        manna -= 10;
+    }
+
+    @Override
+    public void showStats() {
+        System.out.println("I am powerful!");
+        super.showStats();
+    }
+}
